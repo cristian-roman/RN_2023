d593f2bfc6c010faabcb56966eb1be97f4088f4c
Cristian Roman <romancristianroman@yahoo.com>
2023-11-03 14:20:35 +0200

    learning perceptron -- done

--- Tema_1/adalinePerceptron.py
+++ Tema_1/adalinePerceptron.py
@@ -2,15 +2,13 @@ import numpy as np
 
 
 class Perceptron:
-    learning_rate = 0.1
-    total_outputs = 10
 
     def __init__(self, train_data: np.ndarray, train_labels: np.array(np.int8), expected_output: np.uint):
         self.train_data = train_data
         self.expected_output = expected_output
         self.train_labels = self.normalize_labels(train_labels)
-        self.weights = np.random.rand(np.shape(train_data)[1] * Perceptron.total_outputs)
-        self.biases = np.random.rand(Perceptron.total_outputs)
+        self.weights = np.random.rand(np.shape(train_data)[1], 1)
+        self.bias = np.random.rand(1, 1)
 
     def normalize_labels(self, train_labels: np.ndarray) -> np.ndarray:
 
@@ -24,34 +22,96 @@ class Perceptron:
 
         return normalized_labels
 
-    def train_mini_batching(self):
-        mini_batch_size = 500
-        for i in range(0, self.train_data.shape[0], mini_batch_size):
-            (to_update_weights, to_update_biases) = self.__train(self.train_data[i:i + mini_batch_size],
-                                                                 self.train_labels[i:i + mini_batch_size])
-            self.weights = self.weights + to_update_weights
-            self.biases = self.biases + to_update_biases
+    def train(self):
+        batch_size = 1000
+        epoch = 1
+        epsilon = 0.0001
+        while True:
+            learning_rate = get_learning_rate_for_this_epoch(epoch)
+            if learning_rate < epsilon:
+                break
+            self.shuffle_data()
+            for i in range(0, self.train_data.shape[0], batch_size):
+                (to_update_weights, to_update_biases) = self.__train_batch(self.train_data[i:i + batch_size],
+                                                                           self.train_labels[i:i + batch_size],
+                                                                           learning_rate)
+                self.weights = self.weights + to_update_weights
+                self.bias = self.bias + to_update_biases
 
-    def __train(self, train_data: np.ndarray, train_labels: np.array(np.int8)) -> (np.ndarray, np.ndarray):
+            epoch += 1
+
+    def shuffle_data(self):
+        combined_data = np.concatenate((self.train_data, self.train_labels.reshape(-1, 1)), axis=1)
+        np.random.shuffle(combined_data)
+        self.train_data = combined_data[:, :-1]
+        self.train_labels = combined_data[:, -1].astype(np.int8)
+
+    def __train_batch(self, train_data: np.ndarray, train_labels: np.array(np.int8), learning_rate) -> (
+            np.ndarray, np.ndarray):
+
+        mini_batch_size = 100
+        shuffle_data(train_data, train_labels)
+
         to_update_weights = np.zeros(self.weights.shape)
-        to_update_biases = np.zeros(self.biases.shape)
+        to_update_biases = 0
 
         for i in range(train_data.shape[0]):
-            (weights_modification, biases_modification) = self.__train_with_one(train_data[i], train_labels[i])
+            (weights_modification, biases_modification) = self.__train_with_one(
+                train_data[i].reshape(1, np.shape(train_data)[1]), train_labels[i], learning_rate)
 
             to_update_weights += weights_modification
             to_update_biases += biases_modification
 
         return to_update_weights, to_update_biases
 
-    def __train_with_one(self, train_data: np.ndarray, train_label: np.int8) -> (np.ndarray, np.ndarray):
+    def __train_with_one(self, train_data: np.ndarray, train_label: np.int8, learning_rate) -> (np.ndarray, np.float32):
 
-        neuron_value = np.dot(train_data, self.weights) * train_label + self.biases
+        neuron_value_raw = np.dot(train_data, self.weights) * train_label + self.bias
+        neuron_value = stable_sigmoid(neuron_value_raw)
 
-        if neuron_value > 0:  # classified correctly
-            error = train_label - neuron_value
+        if train_label == self.expected_output:
+            error = 1 - neuron_value
+        else:
+            error = 0 - neuron_value
 
-            weights_modification = train_data * self.learning_rate * error
-            biases_modification = self.learning_rate * error
+        weights_modification = (train_data * learning_rate * error).reshape(self.weights.shape)
+        bias_modification = learning_rate * error
 
-        return weights_modification, biases_modification
+        return weights_modification, bias_modification
+
+    def get_perception(self, data: np.ndarray) -> float:
+        return stable_sigmoid(np.dot(data, self.weights) + self.bias)
+
+    def get_accuracy(self):
+        correct_predictions = 0
+        for data in zip(self.train_data, self.train_labels):
+            perception = self.get_perception(data[0])
+            if perception > 0.5:
+                if data[1] == self.expected_output:
+                    correct_predictions += 1
+            else:
+                if data[1] != self.expected_output:
+                    correct_predictions += 1
+
+        return correct_predictions / self.train_data.shape[0]
+
+
+def stable_sigmoid(x):
+    if x >= 0:
+        return 1 / (1 + np.exp(-x))
+    else:
+        return np.exp(x) / (1 + np.exp(x))
+
+
+def get_learning_rate_for_this_epoch(epoch):
+    initial_lr = 0.5
+    k = 0.2  # Adjust this value for the desired rate of decay
+    return initial_lr * 1 / (1 + np.exp(k * epoch))
+
+
+def shuffle_data(data: np.ndarray, labels: np.array(np.int8)):
+    combined_data = np.concatenate((data, labels.reshape(-1, 1)), axis=1)
+    np.random.shuffle(combined_data)
+    data = combined_data[:, :-1]
+    labels = combined_data[:, -1].astype(np.int8)
+    return data, labels
--- Tema_1/main.py
+++ Tema_1/main.py
@@ -12,5 +12,28 @@ test_labels_path = 'dataset/t10k-labels-idx1-ubyte.gz'
 if __name__ == '__main__':
     trainImages = imgLoader.ImageLoader(train_images_path, train_labels_path)
 
-    perceptron = PerceptronModel.Perceptron(trainImages.data, trainImages.labels, np.uint(0))
-    perceptron.train_mini_batching()
+    perceptronList = []
+    for i in range(10):
+        perceptron = PerceptronModel.Perceptron(trainImages.data, trainImages.labels, np.uint(i))
+        perceptron.train()
+        print(f'Perceptron {i} trained')
+        perceptronList.append(perceptron)
+
+    testImages = imgLoader.ImageLoader(test_images_path, test_labels_path)
+
+    correct_predictions = 0
+    for (data, label) in zip(testImages.data, testImages.labels):
+        best_prediction = -1
+        best_prediction_probability = -1
+
+        for perceptron in perceptronList:
+            prediction = perceptron.get_perception(data)
+
+            if prediction > best_prediction_probability:
+                best_prediction = perceptron.expected_output
+                best_prediction_probability = prediction
+
+        if best_prediction == label:
+            correct_predictions += 1
+
+    print(f'Accuracy: {correct_predictions / testImages.data.shape[0]}')
