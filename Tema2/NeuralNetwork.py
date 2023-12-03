a51ebd19c2c3c03b4d9fdecbdd3ac2925f585145
Cristian Roman <romancristianroman@yahoo.com>
2023-12-03 17:37:58 +0200

    accuracy improved

--- Tema2/NeuralNetwork.py
+++ Tema2/NeuralNetwork.py
@@ -2,6 +2,13 @@ import torch.nn as nn
 
 
 class NeuralNetwork(nn.Module):
+
+    @staticmethod
+    def init_weights(m):
+        if type(m) is nn.Linear:
+            nn.init.xavier_uniform_(m.weight)
+            m.bias.data.fill_(0.01)
+
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.linear_relu_stack = nn.Sequential(
@@ -12,6 +19,8 @@ class NeuralNetwork(nn.Module):
             nn.Linear(532, 10),
             nn.Softmax(dim=1)
         )
+
+        self.apply(self.init_weights)
 
     def forward(self, x):
         x = x.reshape(-1, 28 * 28)
--- Tema2/main.py
+++ Tema2/main.py
@@ -5,6 +5,40 @@ from torchvision import datasets, transforms
 from NeuralNetwork import NeuralNetwork
 
 
+def calculate_predictions(loader, model, device):
+    all_labels = []
+    all_predictions = []
+
+    with torch.no_grad():
+        for batch in loader:
+            inputs, labels = batch
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs, dim=1)
+
+            all_labels.extend(labels.cpu().numpy())
+            all_predictions.extend(predicted.cpu().numpy())
+
+    return all_labels, all_predictions
+
+
+def compute_f1_accuracy(labels, predictions):
+    true_positives = sum((l == 1 and p == 1) for l, p in zip(labels, predictions))
+    false_positives = sum((l == 0 and p == 1) for l, p in zip(labels, predictions))
+    false_negatives = sum((l == 1 and p == 0) for l, p in zip(labels, predictions))
+
+    precision = true_positives / (true_positives + false_positives + 1e-10)
+    recall = true_positives / (true_positives + false_negatives + 1e-10)
+
+    f1 = 2 * (precision * recall) / (precision + recall + 1e-10)
+    return f1
+
+
+def general_accuracy(labels, predictions):
+    correct = sum((l == p) for l, p in zip(labels, predictions))
+    return correct / len(labels)
+
+
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using device:", device)
@@ -16,13 +50,13 @@ def main():
 
     # Move data loaders to the specified device
     train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True, pin_memory=True, num_workers=20)
-    test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False, pin_memory=True, num_workers=20)
+    test_loader = DataLoader(test_dataset, batch_size=100, shuffle=False, pin_memory=True, num_workers=20)
 
     criterion = nn.CrossEntropyLoss()
-    model = NeuralNetwork().to(device)  # Move the model to the specified device
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    model = NeuralNetwork().to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-1)
 
-    for epoch in range(1000):
+    for epoch in range(100):
         for batch in train_loader:
             inputs, labels = batch
             inputs, labels = inputs.to(device), labels.to(device)
@@ -33,22 +67,31 @@ def main():
             optimizer.step()
 
     print("Training is done!")
-    print("Testing the model...")
-    total_correct = 0
-    total_images = 0
-    confusion_matrix = torch.zeros(10, 10)
-    with torch.no_grad():
-        for batch in test_loader:
-            inputs, labels = batch
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            _, predicted = torch.max(outputs, dim=1)
-            total_images += labels.size(0)
-            total_correct += int((predicted == labels).sum())
-            for i in range(len(labels)):
-                confusion_matrix[labels[i], predicted[i]] += 1
 
-    print("Accuracy: %.2f%%" % (total_correct / total_images * 100))
+    print("Testing the model...")
+    model.eval()
+
+    print("TRAINING SET")
+    all_labels, all_predictions = calculate_predictions(train_loader, model, device)
+
+    # Calculate general accuracy
+    accuracy = general_accuracy(all_labels, all_predictions)
+    print("General Accuracy: %.2f%%" % (accuracy * 100))
+
+    # Calculate F1 score
+    f1 = compute_f1_accuracy(all_labels, all_predictions)
+    print("F1 Accuracy: %.2f%%" % (f1 * 100))
+
+    print("TEST SET")
+    all_labels, all_predictions = calculate_predictions(test_loader, model, device)
+
+    # Calculate general accuracy
+    accuracy = general_accuracy(all_labels, all_predictions)
+    print("General Accuracy: %.2f%%" % (accuracy * 100))
+
+    # Calculate F1 score
+    f1 = compute_f1_accuracy(all_labels, all_predictions)
+    print("F1 Accuracy: %.2f%%" % (f1 * 100))
 
 
 if __name__ == "__main__":
